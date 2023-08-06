b49a458fa97ccb20628d299f187021c561eeb199
mtalaga20 <michael.talaga@hotmail.com>
2023-08-06 15:25:31 -0400

    Add project files.

---
+++ VehicleInformationAPI/Data/VehicleEquipmentContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleInformationAPI.Models.Entities;
+
+namespace VehicleInformationAPI.Data
+{
+    public class VehicleEquipmentContext : DbContext
+    {
+        public VehicleEquipmentContext(DbContextOptions<VehicleEquipmentContext> options) : base(options) { }
+
+        public DbSet<VehicleEquipment> VehicleEquipment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleEquipment>(x =>
+            x.HasKey(x => x.EquipmentName));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
---
+++ VehicleInformationAPI/Models/Entities/VehicleEquipment.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VehicleInformationAPI.Models.Entities
+{
+    public class VehicleEquipment
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string EquipmentName { get; set; }
+        
+        public string Price { get; set; }
+
+        public string EquipmentClass { get; set; }
+
+        public string? ModelYearId { get; set; }
+
+        public string? StyleId { get; set; }
+    }
+}
